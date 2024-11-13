8aaecebd9af2352b25be4a2b5b1bc28a282e66d5
Mostafa Hegazy <mostafaa.hegazy47@gmail.com>
2024-11-13 23:07:55 +0200

    add all files

---
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('books/', views.list_books, name='list_books'),
+    path('library/<int:pk>', views.LibraryDetailView(), name='library_detail'),     
+]
