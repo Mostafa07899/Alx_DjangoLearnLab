6644a3c4c24ef56d4c08e4381b428fbc30b02b81
Mostafa Hegazy <mostafaa.hegazy47@gmail.com>
2024-11-14 13:23:14 +0200

    finished register,login,logout

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 from . import views
 from .views import list_books
+from .views import LoginView, LogoutView, register
 
 
 
 urlpatterns = [
     path('books/', views.list_books, name='list_books'),
-    path('library/<int:pk>', views.LibraryDetailView(), name='library_detail'),     
+    path('library/<int:pk>', views.LibraryDetailView.as_view(), name='library_detail'),   
+    path("login/", LoginView, name="login"),
+    path("logut/", LogoutView, name="logout"),
+    path("register/", views.SignUpView.as_view(), name="register"),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,6 +2,12 @@ from django.shortcuts import render
 from django.views.generic.detail import DetailView
 from .models import Book
 from .models import Library
+from django.contrib.auth import views as auth_views
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import redirect
+from django.contrib.auth import login
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView
 
 # Create your views here.
 
@@ -15,5 +21,23 @@ class LibraryDetailView(DetailView):
     context_object_name = 'library'
 
 
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("home")
+    else:
+        form = UserCreationForm()
+        return render(request, "register.html", {"form": form})
+    
 
+class SignUpView(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy("login")
+    template_name = "register.html"
+    
 
+LoginView = auth_views.LoginView.as_view(template_name="login.html")
+LogoutView = auth_views.LogoutView.as_view(template_name="logut.html")
