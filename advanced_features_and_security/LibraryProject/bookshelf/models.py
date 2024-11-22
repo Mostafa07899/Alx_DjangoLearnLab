77be7583e37ab970c2ff50f34f4d2d51cb8e6654
Mostafa Hegazy <mostafaa.hegazy47@gmail.com>
2024-11-22 21:07:32 +0200

    did first part

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.contrib.auth.models import User, Group
+
 
 # Create your models here.
 
@@ -43,3 +45,20 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.username
+    
+
+class Article(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        permissions = [ 
+            ("can_view", "Can view article"),
+            ("can_create", "Can create article"),
+            ("can_edit", "Can edit article"),
+            ("can_delete", "Can delete article"),
+        ]
+
+    def __str__(self):
+        return self.title
--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import permission_required
+from .models import Article
+
 
 # Create your views here.
+
+@permission_required('myapp.can_view', raise_exception=True)
+def view_items(request):
+    # Logic for viewing items
+    return render(request, 'view_items.html')
+
+@permission_required('myapp.can_edit', raise_exception=True)
+def edit_item(request, pk):
+    # Logic for editing an item
+    return render(request, 'edit_item.html')
