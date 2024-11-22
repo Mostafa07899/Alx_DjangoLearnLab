58f1a85cf38108ec67e17ca62194bf81afe5167c
Mostafa Hegazy <mostafaa.hegazy47@gmail.com>
2024-11-22 15:44:28 +0200

    created users

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import Book
+from .models import CustomeUser
+from django.contrib.auth.admin import UserAdmin 
 
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'publication_year')  # Display fields in list view
@@ -7,3 +9,16 @@ class BookAdmin(admin.ModelAdmin):
     list_filter = ('publication_year',)  # Enable filtering
 
 admin.site.register(Book, BookAdmin)
+
+
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomeUser
+    fieldsets = UserAdmin.fieldsets + (
+        ('Additional Info', {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ('Additional Info', {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+
+admin.site.register(CustomeUser, CustomUserAdmin)
