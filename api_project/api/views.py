cda2decbb30e6bc052db5cf49c5252c4d73445f1
Mostafa Hegazy <mostafaa.hegazy47@gmail.com>
2024-12-01 23:34:59 +0200

    add viewsets and test crud operations

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework.generics import ListAPIView
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework.viewsets import ModelViewSet
 
 
 # Create your views here.
@@ -9,3 +10,9 @@ from .serializers import BookSerializer
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer 
+
+
+
+class BookViewSet(ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
