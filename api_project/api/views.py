f64290171bfae0775c82293a538f5fc6b770b2c8
Mostafa Hegazy <mostafaa.hegazy47@gmail.com>
2024-12-01 23:46:53 +0200

    add permissions

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -3,6 +3,7 @@ from rest_framework.generics import ListAPIView
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework.viewsets import ModelViewSet
+from rest_framework_permissions import IsAuthenticated
 
 
 # Create your views here.
@@ -16,3 +17,4 @@ class BookList(generics.ListAPIView):
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
