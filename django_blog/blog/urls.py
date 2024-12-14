696a6dc71e72598708c29b94768b0c9778138cc9
Mostafa Hegazy <mostafaa.hegazy47@gmail.com>
2024-12-14 17:52:01 +0200

    coreected some errors

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -16,6 +16,6 @@ urlspatterns = [
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
     path('posts/<int:pk>/comments/new/', CommentCreateView.as_view(), name='comment_create'),
-    path('comments/<int:pk>/update/', CommentUpdateView.as_view(), name='comment_edit'),
-    path('comments/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment_delete'),
+    path('comment/<int:pk>/update/', CommentUpdateView.as_view(), name='comment_edit'),
+    path('comment/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment_delete'),
 ]
