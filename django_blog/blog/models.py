0b5e54295462f20f63bb833fecf57d9094eea2a6
Mostafa Hegazy <mostafaa.hegazy47@gmail.com>
2024-12-14 16:01:35 +0200

    done all

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -13,3 +13,13 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+    
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='profile_pics', default='default.jpg')
+    bio = models.TextField(blank=True)
+
+    def __str__(self):
+        return f"{self.user.username} Profile"
+    
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.views import LoginView, LogoutView
+from .forms import UserRegisterForm
+from django.contrib.auth.decorators import login_required
+from .models import Profile
+
 
 # Create your views here.
+ 
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('profile')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'blog/register.html', {'form': form})
+
+
+@login_required
+def profile(request):
+    if request.method == 'POST':
+        pass
+    return render(request, 'blog/profile.html')
