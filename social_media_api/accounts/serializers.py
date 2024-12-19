e970e479e6fe15e0f055b88d37fdc7c9690b1b6f
Mostafa Hegazy <mostafaa.hegazy47@gmail.com>
2024-12-19 15:33:07 +0200

    fixed some issues

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -7,11 +7,12 @@ from rest_framework.authtoken.models import Token
 User = get_user_model()
 
 class UserSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True, required=True)
+    password = serializers.CharField()
 
     class Meta:
         model = CustomUser
         fields = ['id', 'username', 'email', 'bio', 'profile_picture', 'followers']
+        extra_kwargs = {'password': {'write_only': True}}
 
         def create(self, validated_data):
             user = User.objects.create_user(**validated_data)
