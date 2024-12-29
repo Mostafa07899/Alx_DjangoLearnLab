23df7996adf13e86653dbe69ee3ea7e0a8ff718d
Mostafa Hegazy <mostafaa.hegazy47@gmail.com>
2024-12-29 20:43:49 +0200

    made some corrections

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -56,7 +56,7 @@ class LikePostView(generics.GenericAPIView):
 
     def post(self, request, pk):
         try:
-            post = get_object_or_404(Post, pk=pk)
+            post = generics.get_object_or_404(Post, pk=pk)
             like, created = Like.objects.get_or_create(user=request.user, post=post)
 
             if not created:
